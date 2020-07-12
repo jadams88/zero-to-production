15d793ae93acaa1b5693280b7e17bc78bc8baf52
Jonathon Adams <24870903+jonathonadams@users.noreply.github.com>
2020-07-12 21:36:14 +0930

    feat: basic functionality

--- libs/demo/secure-todos/src/lib/demo-secure-todos.module.ts
+++ libs/demo/secure-todos/src/lib/demo-secure-todos.module.ts
@@ -3,9 +3,10 @@ import { ExampleTodosComponent } from './example-todos/example-todos.component';
 import { DemoSecureTodosRoutingModule } from './demo-secure-todos-routing.module';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { SelectApiComponent } from './select-api/select-api.component';
 
 @NgModule({
   imports: [CommonModule, MatButtonModule, DemoSecureTodosRoutingModule],
-  declarations: [ExampleTodosComponent],
+  declarations: [ExampleTodosComponent, SelectApiComponent],
 })
 export class DemoSecureTodosModule {}
---
+++ libs/demo/secure-todos/src/lib/select-api/select-api.component.ts
@@ -0,0 +1,45 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ApiService, GraphQLService } from '@ztp/common/data-access';
+import { DemoApiService, DemoGraphQLService } from '@ztp/demo/data-access';
+import { timer, Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { switchMap, catchError, map } from 'rxjs/operators';
+
+@Component({
+  selector: 'ztp-select-api',
+  templateUrl: './select-api.component.html',
+  styleUrls: ['./select-api.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SelectApiComponent {
+  k8s = 'https://api.zero-to-production.dev';
+  aws = 'https://fns.zero-to-production.dev';
+
+  k8sStatus$: Observable<boolean>;
+  awsStatus$: Observable<boolean>;
+
+  constructor(
+    private http: HttpClient,
+    private api: ApiService,
+    private gql: GraphQLService
+  ) {
+    this.k8sStatus$ = this.pingForStatus(this.k8s);
+    this.awsStatus$ = this.pingForStatus(this.aws);
+  }
+
+  pingForStatus(url: string): Observable<boolean> {
+    return timer(1000, 3000).pipe(
+      switchMap(() =>
+        this.http.get(`${url}/healthz`, { responseType: 'text' }).pipe(
+          map(() => true),
+          catchError((e) => of(false))
+        )
+      )
+    );
+  }
+
+  selectApi(url: string) {
+    (this.api as DemoApiService).apiUrl = `${url}/api`;
+    (this.gql as DemoGraphQLService).graphQLUrl = `${url}/graphql`;
+  }
+}
