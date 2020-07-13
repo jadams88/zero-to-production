d5794673a70cdf2876d339b2babf92cd2684d2be
Jonathon Adams <24870903+jonathonadams@users.noreply.github.com>
2020-07-13 16:52:40 +0930

    feat: add status indicator

--- libs/demo/secure-todos/src/lib/demo-secure-todos.module.ts
+++ libs/demo/secure-todos/src/lib/demo-secure-todos.module.ts
@@ -4,9 +4,14 @@ import { DemoSecureTodosRoutingModule } from './demo-secure-todos-routing.module
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { SelectApiComponent } from './select-api/select-api.component';
+import { DemoApiStatusComponent } from './status/status.component';
 
 @NgModule({
   imports: [CommonModule, MatButtonModule, DemoSecureTodosRoutingModule],
-  declarations: [ExampleTodosComponent, SelectApiComponent],
+  declarations: [
+    ExampleTodosComponent,
+    SelectApiComponent,
+    DemoApiStatusComponent,
+  ],
 })
 export class DemoSecureTodosModule {}
