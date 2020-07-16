56d0def334cad05b905020cf0e45ab1276ca512a
Jonathon Adams <24870903+jonathonadams@users.noreply.github.com>
2020-07-16 14:46:46 +0930

    chore: fix failing test for select-api component

--- libs/demo/secure-todos/src/lib/select-api/select-api.component.spec.ts
+++ libs/demo/secure-todos/src/lib/select-api/select-api.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectApiComponent } from './select-api.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ApiService, GraphQLService } from '@ztp/common/data-access';
+import { HttpClient } from '@angular/common/http';
 
 describe('SelectApiComponent', () => {
   let component: SelectApiComponent;
@@ -9,13 +12,18 @@ describe('SelectApiComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [SelectApiComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: HttpClient, useValue: {} },
+        { provide: ApiService, useValue: {} },
+        { provide: GraphQLService, useValue: {} },
+      ],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SelectApiComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
